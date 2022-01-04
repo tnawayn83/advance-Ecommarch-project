2014cfa80f292694628200a93acd7176f80869b8
md tariqul islam <tnawayn83@yahoo.com>
2022-01-04 23:27:36 +0300

    Hot Deals Product Show
    Hot Deals Product Show

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -91,14 +91,28 @@
 
           <!-- ============================================== HOT DEALS ============================================== -->
           <div class="sidebar-widget hot-deals wow fadeInUp outer-bottom-xs">
-            <h3 class="section-title">hot deals</h3>
+            <h3 class="section-title"> @if(session()->get('language') == 'bangla') গরম হচ্ছে @else hot deals @endif </h3>
             <div class="owl-carousel sidebar-carousel custom-carousel owl-theme outer-top-ss">
+
+                @foreach($hot_deals as $product)
               <div class="item">
                 <div class="products">
                   <div class="hot-deal-wrapper">
-                    <div class="image"> <img src="{{asset('frontend/assets/images/hot-deals/p25.jpg')}}" alt=""> </div>
-                    <div class="sale-offer-tag"><span>49%<br>
-                      off</span></div>
+                    <div class="image"> <img src="{{ asset($product->product_thambnail) }}" alt=""> </div>
+
+                      @php
+                      $amount = $product->selling_price - $product->discount_price;
+                      $discount = ($amount/$product->selling_price) * 100;
+                      @endphp
+
+                            @if ($product->discount_price == NULL)
+                              <div class="tag new"><span>new</span></div>
+                            @else
+                            <div class="sale-offer-tag"><span>{{ round($discount) }}%<br>
+                                  off</span></div>
+                            @endif
+
+
                     <div class="timing-wrapper">
                       <div class="box-wrapper">
                         <div class="date box"> <span class="key">120</span> <span class="value">DAYS</span> </div>
@@ -117,9 +131,13 @@
                   <!-- /.hot-deal-wrapper -->
 
                   <div class="product-info text-left m-t-20">
-                    <h3 class="name"><a href="detail.html">Floral Print Buttoned</a></h3>
+                    <h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}">@if(session()->get('language') == 'bangla') {{ $product->product_name_bn }} @else {{ $product->product_name_en }} @endif</a></h3>
                     <div class="rating rateit-small"></div>
-                    <div class="product-price"> <span class="price"> $600.00 </span> <span class="price-before-discount">$800.00</span> </div>
+                    @if ($product->discount_price == NULL)
+ <div class="product-price"> <span class="price"> TK{{ $product->selling_price }} </span>  </div>
+     @else
+ <div class="product-price"> <span class="price"> TK{{ $product->discount_price }} </span> <span class="price-before-discount">TK{{ $product->selling_price }}</span> </div>
+     @endif
                     <!-- /.product-price -->
 
                   </div>
@@ -137,94 +155,7 @@
                   <!-- /.cart -->
                 </div>
               </div>
-              <div class="item">
-                <div class="products">
-                  <div class="hot-deal-wrapper">
-                    <div class="image"> <img src="{{asset('frontend/assets/images/hot-deals/p5.jpg')}}" alt=""> </div>
-                    <div class="sale-offer-tag"><span>35%<br>
-                      off</span></div>
-                    <div class="timing-wrapper">
-                      <div class="box-wrapper">
-                        <div class="date box"> <span class="key">120</span> <span class="value">Days</span> </div>
-                      </div>
-                      <div class="box-wrapper">
-                        <div class="hour box"> <span class="key">20</span> <span class="value">HRS</span> </div>
-                      </div>
-                      <div class="box-wrapper">
-                        <div class="minutes box"> <span class="key">36</span> <span class="value">MINS</span> </div>
-                      </div>
-                      <div class="box-wrapper hidden-md">
-                        <div class="seconds box"> <span class="key">60</span> <span class="value">SEC</span> </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- /.hot-deal-wrapper -->
-
-                  <div class="product-info text-left m-t-20">
-                    <h3 class="name"><a href="detail.html">Floral Print Buttoned</a></h3>
-                    <div class="rating rateit-small"></div>
-                    <div class="product-price"> <span class="price"> $600.00 </span> <span class="price-before-discount">$800.00</span> </div>
-                    <!-- /.product-price -->
-
-                  </div>
-                  <!-- /.product-info -->
-
-                  <div class="cart clearfix animate-effect">
-                    <div class="action">
-                      <div class="add-cart-button btn-group">
-                        <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
-                        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-                      </div>
-                    </div>
-                    <!-- /.action -->
-                  </div>
-                  <!-- /.cart -->
-                </div>
-              </div>
-              <div class="item">
-                <div class="products">
-                  <div class="hot-deal-wrapper">
-                    <div class="image"> <img src="{{asset('frontend/assets/images/hot-deals/p10.jpg')}}" alt=""> </div>
-                    <div class="sale-offer-tag"><span>35%<br>
-                      off</span></div>
-                    <div class="timing-wrapper">
-                      <div class="box-wrapper">
-                        <div class="date box"> <span class="key">120</span> <span class="value">Days</span> </div>
-                      </div>
-                      <div class="box-wrapper">
-                        <div class="hour box"> <span class="key">20</span> <span class="value">HRS</span> </div>
-                      </div>
-                      <div class="box-wrapper">
-                        <div class="minutes box"> <span class="key">36</span> <span class="value">MINS</span> </div>
-                      </div>
-                      <div class="box-wrapper hidden-md">
-                        <div class="seconds box"> <span class="key">60</span> <span class="value">SEC</span> </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- /.hot-deal-wrapper -->
-
-                  <div class="product-info text-left m-t-20">
-                    <h3 class="name"><a href="detail.html">Floral Print Buttoned</a></h3>
-                    <div class="rating rateit-small"></div>
-                    <div class="product-price"> <span class="price"> $600.00 </span> <span class="price-before-discount">$800.00</span> </div>
-                    <!-- /.product-price -->
-
-                  </div>
-                  <!-- /.product-info -->
-
-                  <div class="cart clearfix animate-effect">
-                    <div class="action">
-                      <div class="add-cart-button btn-group">
-                        <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
-                        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-                      </div>
-                    </div>
-                    <!-- /.action -->
-                  </div>
-                  <!-- /.cart -->
-                </div>
-              </div>
+              @endforeach
             </div>
             <!-- /.sidebar-widget -->
           </div>
@@ -932,7 +863,7 @@
             <div class="more-info-tab clearfix ">
 
 
-              <h3 class="new-product-title pull-left">New Products</h3>
+              <h3 class="new-product-title pull-left">@if(session()->get('language') == 'bangla') নতুন পণ্য @else New Products @endif </h3>
               <ul class="nav nav-tabs nav-tab-line pull-right" id="new-products-1">
                 <li class="active"><a data-transition-type="backSlide" href="#all" data-toggle="tab">All</a></li>
                 @foreach($categories as $category)
